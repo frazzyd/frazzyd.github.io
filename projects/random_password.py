17b47d2593585c7babf93ad7951d8792e747b8d2
frazzyd <fraser.dumayne@gmail.com>
2017-07-04 22:21:17 +0100

    update projects

---
+++ projects/random_password.py
@@ -0,0 +1,34 @@
+# Script:    passwordgenerator.py
+# Desc:      Create a random password of a given length.
+# Author:    Fraser Dumayne
+# Created:   12/09/2014
+# Updated:	 04/01/2017 - Added boilerplate and main
+######################################################################################################
+
+import random, sys
+
+# Create string with possible characters
+passChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz0123456789!?_-@£$%^&*+="
+
+def passCreate(passLength):
+	
+	password = [] # Create empty list
+	
+	for character in range(passLength): # Loop based on size given by user
+		password.append(random.choice(passChars)) # Choose a random character from passChars and add to the empty list
+	print ''.join(password) # Print the password by joining every item in the list into a string
+	
+	return password
+
+def main():
+	sys.argv.append(12)
+	
+	if len(sys.argv) != 2:
+		print 'Please input the length of password you would like'
+		return
+		
+	passCreate(sys.argv[1])
+
+if __name__ == '__main__':
+	main()
+
---
+++ projects/webpage_search.py
@@ -0,0 +1,40 @@
+# Script:    webpage_search.py
+# Desc:      Search a given webpage for a given search term using BeautifulSoup.
+# Author:    Fraser Dumayne
+# Created:   20/10/2014
+# Updated:   04/01/2017 - Added boilerplate, main and error handling
+######################################################################################################
+
+
+import sys, bs4, urllib2
+        
+def webSearch(url, searchTerm):
+    
+    try:
+        webpage = urllib2.urlopen(url).read() # Open the webpage
+        pageContents = bs4.BeautifulSoup(webpage, "html.parser") # Collect contennts of webpage
+        pageOutput = pageContents.get_text()# Get just the plaintext of the webpage
+
+        if searchTerm in pageOutput: # Search for the word on the webpage
+            print "Your search term, " + searchTerm + ", was successfully located."
+        else:
+            print "Your search term, " + searchTerm + ", was not found."
+    except IOError as error: # Provide error if URL cannot be located
+        print 'Exception: URL not found: ' + str(error)
+    except Exception as error: # Catch all other errors
+        print 'Exception: ' + str(error)
+
+    return
+
+def main():
+    sys.argv.append(r'http://www.fraserdumayne.co.uk')
+    sys.argv.append('Fraser')
+    
+    if len(sys.argv) != 3:
+        print 'Please enter a valid URL (e.g. \'http://www.fraserdumayne.co.uk\') and a valid search term'
+        return
+
+    webSearch(sys.argv[1], sys.argv[2])
+
+if __name__ == '__main__':
+	main()
